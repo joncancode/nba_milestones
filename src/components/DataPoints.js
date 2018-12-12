c1ebc9f203b98a6705d6ba2e4899da8ecb0928c1
joncancode <jc.haines19@gmail.com>
2018-12-12 00:57:24 -0500

    re-ordering of cards

--- src/components/DataPoints.js
+++ src/components/DataPoints.js
@@ -1,112 +1,53 @@
 import React, { Component } from 'react';
 import points from '../data/DataPoints.json';
 import Card from '@material-ui/core/Card';
-import axios from 'axios';
+import LebronTracker from './LebronTracker';
 
 const cardStyle = {
-  notActive: {},
-  isActive: {
-    fontWeight: 'bold'
+  notActive: {
+    margin: '0 auto',
+    display: 'inline-block',
+    marginBottom: '10px',
+    width: '20%',
+    textAlign: 'center',
+    padding: '25px',
+  },
+  cardGroup: {
+    width: '100%',
+    bottom: '0',
+    position: 'fixed',
+    border: '2px solid indigo'
   }
 };
 
-
-const lebronApiUrl = `http://data.nba.net/10s/prod/v1/2018/players/2544_profile.json`;
-const dirkApiUrl = `http://data.nba.net/10s/prod/v1/2018/players/1717_profile.json`;
 let pointsArr = points;
 
 export class DataPoints extends Component {
   state = {
-    points: pointsArr,
-    active: [
-    {
-      "name": "LeBron James",
-      "points": 31628,
-      "isActive": true
-    },
-    {
-      "name": "Dirk Nowitzki",
-      "points": 31187,
-      "isActive": true
-    }]
-  }
+    points: pointsArr
+  };
 
   componentWillMount() {
-    console.log('state mount', this.state);
-    this.findActiveScores()
-  }
-
-  findActiveScores(){
-    this.findLebron()
-    this.findDirk()
-  }
-
-  findLebron() {
-    this.setState({ 
-    }, () => {
-      axios
-        .get(`https://cors-anywhere.herokuapp.com/${lebronApiUrl}`)
-        .then(res => {
-          console.log(
-            'lebron total points',
-            res.data.league.standard.stats.careerSummary.points
-          );
-          this.setState({lebronPoints: res.data.league.standard.stats.careerSummary.points})
-        })
-        .catch(error => this.setState({ error, isLoaded: false }));
-    });
-  }
-
-  findDirk() {
-    this.setState({}, () => {
-      axios
-        .get(`https://cors-anywhere.herokuapp.com/${dirkApiUrl}`)
-        .then(res => {
-          this.setState({
-            dirkPoints: res.data.league.standard.stats.careerSummary.points
-          });
-        })
-        .catch(error => this.setState({ error, isLoaded: false }));
+    this.setState({
+      pointLeaders: this.pointLeaders
     });
   }
 
   points = this.state.points.map(function(item, i) {
-    if (item.isActive === false) {
-      return (
-        <Card className="notActive" style={cardStyle.notActive} key={i}>
-          {item.name}: {item.points}
-        </Card>
-      );
-    }
-    if (item.isActive === true) {
-      if (item.name === 'LeBron James') {
-        console.log('hello lebron');
-      }
-
-      return (
-        <Card className="isActive" style={cardStyle.isActive} key={i}>
-          {this.state.active.name}: {this.state.lebronPoints}
-        </Card>
-      );
-    }
+    return (
+      <Card className="notActive" style={cardStyle.notActive} key={i}>
+        {item.name}<br/> {item.points.toLocaleString('en', { useGrouping: true })}
+      </Card>
+    );
   });
 
   render() {
-    console.log('state', this.state);
-    // const points = this.state.points.map(function(item, i) {
-    //   if (item.isActive === false){
-    //   return <Card className='notActive' style={cardStyle.notActive} key={i}>{item.name}: {item.points}</Card>;
-    //   }
-    //   if (item.isActive === true){
-    //     return <Card className='isActive' style={cardStyle.isActive} key={i}>{item.name}: {item.points}</Card>;
-    //     }
-    // });
-
     return (
       <div>
+        <LebronTracker pointTotal={this.lebronPoints} />
+        <div style={cardStyle.cardGroup}>
         {this.points}
-        <p>Lebron's total: {this.state.lebronPoints}</p>
-        <p>Dirk's total: {this.state.dirkPoints}</p>
+      </div>
       </div>
     );
   }
--- src/components/LebronTracker.js
+++ src/components/LebronTracker.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
-export class DataPoints extends Component {
+const lebronApiUrl = `http://data.nba.net/10s/prod/v1/2018/players/2544_profile.json`;
+export class LebronTracker extends Component {
+
+  componentWillMount() {
+    this.findLebron();
+  }
+
+  findLebron() {
+    this.setState({}, () => {
+      axios
+        .get(`https://cors-anywhere.herokuapp.com/${lebronApiUrl}`)
+        .then(res => {
+          console.log(
+            'lebron total points',
+            res.data.league.standard.stats.careerSummary.points
+          );
+          this.setState({
+            lebronPoints: res.data.league.standard.stats.careerSummary.points
+          });
+        })
+        .catch(error => this.setState({ error, isLoaded: false }));
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>THE GOAT: under construction</h2>
+        <h2>THE GOAT: under construction {this.state.lebronPoints}</h2>
       </div>
     )
   }
 }
 
-export default DataPoints
+export default LebronTracker;
