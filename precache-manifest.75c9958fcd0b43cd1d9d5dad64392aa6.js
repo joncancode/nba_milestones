b40adc0b2d19eea21647e08a4b727e200ef75b7b
joncancode <jc.haines19@gmail.com>
2018-12-14 00:26:33 -0500

    Updates

---
+++ precache-manifest.75c9958fcd0b43cd1d9d5dad64392aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27f97377bd48946f4ae5",
+    "url": "/nba_milestones/static/js/runtime~main.27f97377.js"
+  },
+  {
+    "revision": "7326334d301ffda4a07f",
+    "url": "/nba_milestones/static/js/main.7326334d.chunk.js"
+  },
+  {
+    "revision": "4addf5054850cbcea1cb",
+    "url": "/nba_milestones/static/js/1.4addf505.chunk.js"
+  },
+  {
+    "revision": "7326334d301ffda4a07f",
+    "url": "/nba_milestones/static/css/main.82b99e54.chunk.css"
+  },
+  {
+    "revision": "882d6782bb175c4ee9557fed94f76553",
+    "url": "/nba_milestones/index.html"
+  }
+];
